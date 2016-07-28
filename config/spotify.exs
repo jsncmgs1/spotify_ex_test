ac4a16585e499cb36ba73135def7947b0d76dce1
Jason Cummings <jasoncummings86@gmail.com>
2016-07-27 21:37:08 -0400

    Add playlist examples

--- config/spotify.exs
+++ config/spotify.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
 config :spotify_ex,
-  user_id: "12182580077",
+  user_id: "12182580077", # replace with your id here,
   callback_url: "http://localhost:4000/authenticate",
-  scopes: ["playlist-read-private"]
+  scopes: ["playlist-read-private", "playlist-modify-public"]
--- web/controllers/playlist_controller.ex
+++ web/controllers/playlist_controller.ex
@@ -1,11 +1,25 @@
 defmodule SpotifyExTest.PlaylistController do
   use SpotifyExTest.Web, :controller
   plug SpotifyExTest.Plugs.Auth
+  @user_id
 
   def index(conn, _params) do
-    {:ok, playlist} = Spotify.Playlist.featured(conn)
-    render conn, "index.html", profile: profile, another_profile: another_profile
-  end
+    #GET collection
+    {:ok, playlists} = Spotify.Playlist.featured(conn)
 
+    #GET playlist
+    {:ok, playlist} = Spotify.Playlist.get_playlist(conn, "12182580077", "6xQiPOErdJLS3JEUHEC8Y0")
+
+    #POST
+    body = Poison.encode! %{name: "foo", public: true}
+    {:ok, playlist} = Spotify.Playlist.create_playlist(conn, "12182580077", body)
+
+    #PUT
+    :ok = Spotify.Playlist.follow_playlist(conn, "spotify", "103Zi2NG06F9qimASDrszv")
+
+    #DELETE
+
+    render conn, "index.html", playlists: playlists, playlist: playlist
+  end
 
 end
